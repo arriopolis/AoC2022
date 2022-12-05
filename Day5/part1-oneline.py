e1462edf289b3048746f7a76b912704af9082280
arjan <arjan_rekenmachine@hotmail.com>
2022-12-05 10:36:27 +0100

    Added solutions to Day 5.

---
+++ Day5/part1-oneline.py
@@ -0,0 +1,28 @@
+print(next(
+    ''.join([c[-1] for x,c in sorted(stacks.items())])
+    for lines in [open('input.txt').readlines()]
+    for stacks in [{}]
+    for _ in [[
+        None
+        for _ in range(len(lines))
+        if lines[1].rstrip()
+        for _ in [
+            (
+                stacks.update({ctr[0] : []}) if ctr[0] not in stacks else None,
+                stacks[ctr[0]].insert(0, line[1]) if not line[:4] == [' ',' ',' ',' '] else None,
+                [line.pop(0) for _ in range(4)],
+                ctr.append(ctr.pop()+1)
+            )
+            for line in [list(lines.pop(0))]
+            for ctr in [[1]]
+            for _ in range(len(line)//4)
+        ]
+    ]]
+    for _ in [[
+        stacks[d].append(stacks[s].pop())
+        for line in lines[2:]
+        for _,n,_,s,_,d in [line.strip().split()]
+        for n,s,d in [map(int, (n,s,d))]
+        for _ in range(n)
+    ]]
+))
---
+++ Day5/part2.py
@@ -0,0 +1,26 @@
+import sys
+
+lines = sys.stdin.readlines()
+stacks = {}
+while lines[1].rstrip():
+    line = lines.pop(0).rstrip()
+    ctr = 1
+    while line:
+        while line.startswith('    '):
+            ctr += 1
+            line = line[4:]
+        if ctr not in stacks: stacks[ctr] = []
+        stacks[ctr].insert(0, line[1])
+        ctr += 1
+        line = line[4:]
+
+for line in lines[2:]:
+    _,n,_,s,_,d = line.strip().split()
+    n,s,d = map(int, (n,s,d))
+    stacks[d].extend(stacks[s][-n:])
+    stacks[s] = stacks[s][:-n]
+
+s = ''
+for x,c in sorted(stacks.items()):
+    s += c[-1]
+print(s)
