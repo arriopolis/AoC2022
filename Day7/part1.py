eabba6b4c4d6e7e3987e06a9797460f3573da4d3
arjan <arjan_rekenmachine@hotmail.com>
2022-12-18 14:48:19 +0530

    Added solutions to day 7.

---
+++ Day7/part1.py
@@ -0,0 +1,54 @@
+import sys
+
+folder_stack = []
+folder_contents = {}
+folder_tree = {}
+for line in sys.stdin:
+    if not line.strip(): break
+    cmds = line.strip().split()
+    if cmds[0] == '$':
+        # User command
+        if cmds[1] == 'cd':
+            # Change directory
+            if cmds[2] == '..':
+                folder_stack.pop()
+            elif cmds[2] == '/':
+                folder_stack.clear()
+            else:
+                folder_stack.append(cmds[2])
+        elif cmds[1] == 'ls':
+            # List
+            pass
+    else:
+        # List output
+        current_folder = tuple(folder_stack)
+        if current_folder not in folder_contents: folder_contents[current_folder] = {}
+        if current_folder not in folder_tree: folder_tree[current_folder] = {}
+
+        if cmds[0] == 'dir':
+            folder_tree[current_folder][cmds[1]] = None
+        else:
+            size = int(cmds[0])
+            name = cmds[1]
+            folder_contents[current_folder][name] = size
+
+folder_sizes = {}
+
+def calc_folder_sizes(current_folder = tuple()):
+    assert current_folder in folder_tree
+    for x in folder_tree[current_folder]:
+        size = calc_folder_sizes(tuple(list(current_folder) + [x]))
+        folder_tree[current_folder][x] = size
+
+    assert current_folder in folder_contents
+    size = sum(folder_contents[current_folder].values()) + sum(folder_tree[current_folder].values())
+    folder_sizes[current_folder] = size
+    return size
+
+calc_folder_sizes()
+
+t = 0
+for folder,size in folder_sizes.items():
+    if size <= 100000:
+        t += size
+print(t)
